744dab209ae67dfb832f9c126e6a518d834da72a
Khaile <khai.le@akagidital.vn>
2024-07-10 17:41:52 +0700

    sw

---
+++ sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyAmw9TnSlGbKJrUnAHuJQT6yl0gvIq2ep8',
+    messagingSenderId: '364821496539',
+    appId: '1:364821496539:web:380edcbe80a569597542fe',
+    projectId: 'bvbdemo-8faf8',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BSUH1PDDQFBSPLEV97HGG26AH0G',
+    siteid: '9028856153133683c37a942d18e07470'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
